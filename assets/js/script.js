4078cdb13e8bbb34a333fc3e15f54f29d0df5140
Andrew Koster <andrewkoster95@gmail.com>
2021-06-22 18:27:31 -0400

    Added questions to script.js file.

---
+++ assets/js/script.js
@@ -0,0 +1,55 @@
+const
+
+function generateQuiz(questions, quizContainer, highScoreContainer){
+
+    function showQuestions(questions, quizContainer){
+
+        var output = [];
+        var answers;
+    }
+
+    function 
+}
+
+var quizQuestions = [
+    {
+        question: "Which one of these is a Javascript package manager?",
+        answers: {
+            a: "style.js",
+            b: "npm",
+            c: "TypeScript",
+            d: "Node.js"
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "In Javascript, how do you call a function named \"myFunction\"?",
+        answers: {
+            a: "function myFunction()",
+            b: "function = myFunction()",
+            c: "function:myFunction()",
+            d: "function var myFunction()"
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "How do you write \"Hello Word\" in an alert box?",
+        answers: {
+            a: "msgBox(\"Hello World\")",
+            b: "alertBox(\"Hello World\")",
+            c: "alert(\"Hello World\")",
+            d: "msg(\"Hello World\")"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Which operator is used to assign a value to a variable?",
+        answers: {
+            a: "*",
+            b: "-",
+            c: "x",
+            d: "="
+        },
+        correctAnswer: "d"
+    }
+];
