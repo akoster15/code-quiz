bc05152c5b494859e2fe23a33a021cb6c1320df3
Andrew Koster <andrewkoster95@gmail.com>
2021-06-22 19:55:30 -0400

    Added function to start to start the quiz. Also decided to add a separate html file for the highscores to make code easier to work through

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,5 @@
-const
-
-function generateQuiz(questions, quizContainer, highScoreContainer){
-
-    function showQuestions(questions, quizContainer){
-
-        var output = [];
-        var answers;
-    }
-
-    function 
-}
-
-var quizQuestions = [
+//Questions for the quiz
+var questions = [
     {
         question: "Which one of these is a Javascript package manager?",
         answers: {
@@ -53,3 +41,35 @@ var quizQuestions = [
         correctAnswer: "d"
     }
 ];
+
+//Rest of variables for the quiz
+const startButton = document.querySelector('.start_button');
+const quizContainer = document.querySelector('.quiz_container');
+const timeText = document.querySelector('.timer .time_left_text');
+const timeCount = document.querySelector('.timer .time_left_num');
+var myScore = 0;
+var currentQuestion;
+var nextQuestion;
+var timeCount = 50;
+
+//Start Button to start the quiz
+startButton.addEventListener("click", generateQuiz);
+
+//Function that starts the quiz
+function starQuiz(){
+    if(storedScores !==null) {
+        allScores = storedScores;
+    }
+    info.classList.add("d-none")
+    btnStart.classList.add("d-none")
+    timecounter.classList.remove("d-none")
+    quizQuestions.classList.remove("d-none")
+    nextQuestions= questions[currentindex]
+    console.log(nextQuestions.title)
+    
+        displayQuestion(nextQuestions)
+
+    gametime()
+}
+
+function quizTime()
