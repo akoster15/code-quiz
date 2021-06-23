87563ab4e8adcc8d7d1313bc8861ee9591084bb6
Andrew Koster <andrewkoster95@gmail.com>
2021-06-22 23:52:57 -0400

    updated both htmls along with scirpt.js and style.css. about to update README

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,14 +43,24 @@ var questions = [
 ];
 
 //Rest of variables for the quiz
-const startButton = document.querySelector('.start_button');
-const quizContainer = document.querySelector('.quiz_container');
-const timeText = document.querySelector('.timer .time_left_text');
-const timeCount = document.querySelector('.timer .time_left_num');
+var quizQuestions = document.getElementById("quiz-questions");
+var timer = document.getElementById("timer");
+var btnStart = document.getElementById("start-button");
+var timecounter = document.getElementById("timecounter");
 var myScore = 0;
 var currentQuestion;
 var nextQuestion;
-var timeCount = 50;
+var questionAnswers = document.querySelector("question_answers");
+var count = 50;
+var everyScore = [];
+var storedScores = JSON.parse(localStorage.getItem("userData"));
+
+
+//Variables for the highscore page
+var storedHighScores = JSON.parse(localStorage.getItem("userData"));
+var highScoreArea = document.querySelector("#highScoresList");
+var backButton = document.querySelector("#back-Button");
+var clearButton = document.querySelector("#clearScores");
 
 //Start Button to start the quiz
 startButton.addEventListener("click", generateQuiz);
@@ -58,7 +68,7 @@ startButton.addEventListener("click", generateQuiz);
 //Function that starts the quiz
 function starQuiz(){
     if(storedScores !==null) {
-        allScores = storedScores;
+        everyScore = storedScores;
     }
     info.classList.add("d-none")
     btnStart.classList.add("d-none")
@@ -69,7 +79,91 @@ function starQuiz(){
     
         displayQuestion(nextQuestions)
 
-    gametime()
+    startTimer()
 }
 
-function quizTime()
+//Function for quiz timer
+function startTimer(){
+    var timeinterval = setInterval(function(){
+        timer.innerText = count
+         count--;
+        }, 1000);
+}
+
+
+function scorePage(a, b) {
+
+    var userData = {
+        inits: a,
+        userScore: b
+    };
+    allScores.push(userData);
+
+    localStorage.setItem("userData", JSON.stringify(allScores));
+    location.href = "score.html";
+}
+
+function displayQuestion(question){
+    titleitem.innerText=question.title
+    questions.choices.forEach(element => {
+        var button = document.createElement("button")
+        button.className="btn-rp"
+        button.innerText=element
+        questionAnswers.appendChild(button)
+        button.addEventListener("click", displaynextQuestion)
+    });
+}
+
+function displayQuestion(e){
+    currentindex++
+    if(currentIndex < questions.length){
+        correction(e.target.innerText == nextQuestion.answer);
+        question
+    }
+}
+
+function answerStatus(response){
+    if(response){
+        alert.innerText= "Correct!";
+    }else {
+        alert.innerText= "Incorrect!";
+        count = count -10;
+        timer.innerHTML = count;
+    }
+    set
+}
+
+function endQuiz(){
+    myScore.innaText = count
+    addscore.classList.remove("d-none")
+    timecounter.classList.add("d-none")
+    quizQuestions.classList.add("d-none")
+    addscore.classList.remove("d-none")
+}
+
+function displayScores() {
+    if (storedScores !== null) {
+        var scoreList = document.createElement("ol");
+        scoreList.className = "scoreListClass";
+        for (var i = 0; i < storedHighScores.length; i++) {
+            var initials = storedHighScores[i].inits;
+            var scores = storedHighScores[i].userScore
+            var scoreEntry = document.createElement("li");
+            scoreEntry.innerHTML = initials + " - " + scores;
+            scoreList.appendChild(scoreEntry);
+        }
+        highScoresArea.appendChild(scoreList);
+    }
+};
+
+displayScores();
+
+backBtn.addEventListener("click", function () {
+    location.href = "index.html";
+});
+
+clearButton.addEventListener("click", function () {
+    highScoresArea.innerHTML = "";
+    window.localStorage.clear();
+
+});
